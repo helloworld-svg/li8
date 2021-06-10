072372fd55a6b6e98fb73f12940d288db06f715c
徐飞12138 <1399149390@qq.com>
2021-06-10 18:46:20 +0800

    项目

--- src/views/principal/prin.vue
+++ src/views/principal/prin.vue
@@ -1,40 +1,174 @@
 <template>
-  <div id='body_box'>
-    <div id='paper'>
-      <div id='newpaper'></div>
-      <div id='newadva'></div>
-      <div id='newdata'></div>
-      <div id=''></div>
-      <div></div>
+  <div id="body_box">
+    <div id="paper">
+      <div id="newpaper">
+        <h4>最新论文</h4>
+        <ul>
+          <li v-for="(it,x) in papers_item.item" :key="x" v-if="x%2==0">
+            {{it.title}} ----- {{it.author}}
+          </li>
+        </ul>
+      </div>
+      <div id="newpaper2">
+        <h4>最新论文</h4>
+        <ul>
+          <li v-for="(it,x) in papers_item.item" :key="x" v-if="x%2!=0">
+            {{it.title}} ----- {{it.author}}
+          </li>
+        </ul>
+      </div>
+      <div id="newadva">
+        <h4>优秀片段</h4>
+        <div v-html='papers_detail_item.item[0].content'></div>
+      </div>
+      <div id="newdata">
+        <h4>最新上传</h4>
+        <ul>
+          <li v-for="(it,x) in papers_item.item" :key="x">
+            {{it.title}} ----- {{it.createTime | renderTime}}
+          </li>
+        </ul>
+      </div>
+      <div id="limit">
+        <h4>其他</h4>
+      </div>
     </div>
-    <div id='notice'>
-
-    </div>
+    <div id="notice"></div>
   </div>
 </template>
 
 <script>
 export default {
-  
-}
+  data() {
+    return {
+      papers_item: {
+        page: "1",
+        pageSize: "2",
+        item: [],
+      },
+      papers_detail_item: {
+        id: "1",
+        item: [{content:null}],
+      },
+    };
+  },
+  mounted() {
+    this.papers();
+    this.paper_detail();
+  },
+  methods: {
+    papers() {
+      this.$https
+        .get(
+          `/paper/?page=${this.papers_item.page}&pageSize=${this.papers_item.pageSize}`
+        )
+        .then((res) => {
+          this.papers_item.item = res.data;
+        });
+    },
+    paper_detail() {
+      this.$https
+        .get(`/paper/detail/?id=${this.papers_detail_item.id}`)
+        .then((res) => {
+          this.papers_detail_item.item=res.data
+        });
+    },
+  },
+  filters: {
+    renderTime(date) {
+      var dateee = new Date(date).toJSON();
+      return new Date(+new Date(dateee) + 8 * 3600 * 1000)
+        .toISOString()
+        .replace(/T/g, " ")
+        .replace(/\.[\d]{3}Z/, "");
+    },
+  },
+};
 </script>
 
 <style scoped>
-  #body_box{
-    width:1000px;
-    height:1000px;
-    margin:10px auto 0px;
-  }
-  #paper{
-    width:calc(70% - 2px);
-    height:100%;
-    border:1px #000 solid;
-    float:left;
-  }
-  #notice{
-    width:calc(30% - 2px);
-    height:100%;
-    border:1px #000 solid;
-    float:left;
-  }
+#body_box {
+  width: 1000px;
+  height: 1000px;
+  margin: 10px auto 0px;
+}
+#paper {
+  width: calc(70% - 2px);
+  height: 100%;
+  border: 1px #000 solid;
+  float: left;
+}
+#notice {
+  width: calc(30% - 2px);
+  height: 100%;
+  border: 1px #000 solid;
+  float: left;
+}
+#newpaper {
+  width: 40%;
+  height: 30%;
+  border: 1px solid #000;
+  border-top: 5px #ffd75c solid;
+  float: left;
+  margin: 2.5% 0px 0px 6.5%;
+  position: relative;
+}
+#newpaper2 {
+  width: 40%;
+  height: 30%;
+  border: 1px solid #000;
+  float: left;
+  margin: 2.5% 0px 0px 6.5%;
+  position: relative;
+}
+#newadva {
+  width: 40%;
+  height: 30%;
+  border: 1px solid #000;
+  clear: both;
+  float: left;
+  margin: 2.5% 0px 0px 6.5%;
+  position: relative;
+}
+#newadva>div{
+  font-size:10px;
+  margin:10px 0px 0px 0px;
+  overflow: hidden;
+  width:100%;
+  height: 100%;
+}
+#newdata {
+  width: 40%;
+  height: 30%;
+  border: 1px solid #000;
+  float: left;
+  margin: 2.5% 0px 0px 6.5%;
+  position: relative;
+}
+#limit {
+  width: 86.5%;
+  height: 30%;
+  border: 1px solid #000;
+  float: left;
+  clear: both;
+  margin: 5% 0px 0px 6.5%;
+  position: relative;
+}
+h4 {
+  margin: 0px;
+  color: #888;
+  position: absolute;
+  top: -13px;
+  left: 10px;
+  background: #fff;
+}
+ul {
+  margin: 0px;
+  padding: 0px 10px;
+}
+li {
+  list-style-type: none;
+  margin: 10px 0px 0px 0px;
+  font-size: 12px;
+}
 </style>
